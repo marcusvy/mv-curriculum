06ad52d97f00e7ab4bbcf4df48fe02e19ae50ba4
Marcus Vinicius Cardoso <marcusvy@gmail.com>
2021-05-16 23:36:14 -0400

    (deploy) gh-pages

--- gh-page.ps1
+++ gh-page.ps1
@@ -1,5 +1,6 @@
 rm .\docs\
 ng build --configuration production --base-href https://marcusvy.github.io/mv-curriculum/  --output-path docs
+cp .\docs\index.html .\docs\404.html
 git add .
 git commit -am "(deploy) gh-pages"
 git push --all
